7492b858f01b6bb9224887a5325a7389e2ae4b99
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-12 15:03:29 +0900

    ユーザー管理機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,2 @@
 class UsersController < ApplicationController
-  def create
-   @user = User.new
-    if @user.save
-      redirect_to root_path(@user)
-    else
-      render :new
-    end
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :email,           uniqueness: { case_sensitive: false }
   validates :password,        format: {with: VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   validates :nickname,        presence: true
-  validates :first_name,      presence: true, format: {with: /\A[一-龥]+\z/}
-  validates :last_name,       presence: true, format: {with: /\A[一-龥]+\z/}
-  validates :first_name_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :last_name_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
   validates :birth_date,      presence: true
+
+  with_options presence: true do
+  validates :first_name,      format: {with: /\A[一-龥]+\z/}
+  validates :last_name,       format: {with: /\A[一-龥]+\z/}
+  validates :first_name_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+  validates :last_name_kana,  format: {with: /\A[ァ-ヶー－]+\z/}
+  end
 end

9b76cb60fb8fad84067ad6d97002f887cee82ae3
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-13 15:15:59 +0900

    商品機能の実装（testコードなし）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,31 @@
 class ItemsController < ApplicationController
+  before_action :move_to_sign_in, only: [:new]
   def index
+    @item = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path(@item)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name,:item_info,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:image).merge(user_id: current_user.id)
+  end
+
+  def move_to_sign_in
+    unless user_signed_in?
+      redirect_to  new_user_session_path
+    end
   end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,26 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  # has_one_attached :image
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :item_category
+  belongs_to :item_prefecture
+  belongs_to :item_sales_status
+  belongs_to :item_scheduled_delivery
+  belongs_to :item_shipping_fee_status
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :item_category_id
+    validates :item_sales_status_id
+    validates :item_shipping_fee_status_id
+    validates :item_prefecture_id
+    validates :item_scheduled_delivery_id
+  end
+  with_options presence: true do
+    validates :item_name
+    validates :item_info
+    validates :item_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}, format: { with: /\A[0-9]+\z/}
+    validates :image
+  end
+  has_one_attached :image
+end

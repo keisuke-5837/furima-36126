dcc8823b3944897b5da8cb0964d475c031877ed5
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-14 12:58:29 +0900

    商品一覧機能の修正３

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,11 +26,5 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name,:item_info,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:image).merge(user_id: current_user.id)
   end
-
-  def authenticate_user!
-    unless user_signed_in?
-      redirect_to  new_user_session_path
-    end
-  end
 end
 

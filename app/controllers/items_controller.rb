228219bd3c58d0e1f5b8ef27aedf9c061067bfe4
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-14 19:40:21 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit]
+  before_action :froce_redirect_unless_my_item, only: [:edit]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -18,12 +21,39 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:item_name,:item_info,:item_category_id,:item_sales_status_id,:item_shipping_fee_status_id,:item_prefecture_id,:item_scheduled_delivery_id,:item_price,:image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+  def froce_redirect_unless_my_item
+    if @item.user != current_user
+      redirect_to root_path
+    end
   end
 end

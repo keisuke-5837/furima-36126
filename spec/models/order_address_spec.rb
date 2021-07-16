115f349c4df9f220d2b9b57c498ea08c6ec417c8
keisuke-5837 <86215950+keisuke-5837@users.noreply.github.com>
2021-07-16 18:37:23 +0900

    購入機能の修正２

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -18,6 +18,16 @@ RSpec.describe OrderAddress, type: :model do
       end
     end
     context '商品購入できない時' do
+      it 'item_idが空では登録できない' do
+        @order_address.item_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'user_idが空では登録できない' do
+        @order_address.user_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
       it 'postal_codeが空では登録できない' do
         @order_address.postal_code = ''
         @order_address.valid?
